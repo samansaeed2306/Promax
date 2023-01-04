3b23ce19f70a9d1d9cff0a8e008bb5d35496b90c
samansaeed2306 <samansaeed2306@gmail.com>
2023-01-04 19:07:23 +0500

    Get post

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const express =require("express");
-const mongoose =require("mongoose")
+const mongoose =require("mongoose");
+const app=express();
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send("<h1>Saman</h1>");
+})
+
+app.post('/',(req,res)=>{
+    
+})
